2cbad8b5bde0164a9ca177df215fc447b7d7acf4
OCC111 <15600686330@163.com>
2018-05-22 14:39:47 +0800

    hello,zidian

---
+++ dic1.py
@@ -0,0 +1,18 @@
+dic1={'name':'邵广超','age':18,'job':'程序员','qq':55648,'mail':'15656@163.com'}
+dic1['name']
+dic1.get('name')
+
+
+list1=[5,'a','t']
+
+k_list1 = dic1.keys()
+print (dic1.keys())
+print (k_list1)
+v_list1 = dic1.values()
+print (dic1.values())
+print (v_list1)
+
+
+kv_list1 = dic1.items()
+print (dic1.items())
+print (kv_list1)
